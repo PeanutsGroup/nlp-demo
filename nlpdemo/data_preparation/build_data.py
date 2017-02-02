bc5fbf7515f152c5cedcc6e0158e2f5bb258f003
JuneQ Cheung <a1624jz@aiit.ac.jp>
2017-02-02 20:37:18 +0900

    Modify data_preparation module

--- nlpdemo/data_preparation/build_data.py
+++ nlpdemo/data_preparation/build_data.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 #
 
-from openpyxl import load_workbook
+import os
 import re
+from openpyxl import load_workbook
 
-def retrieve_contents(infile, outfile):
-    wb = load_workbook(infile)
-    ws = wb.active
-    cells = ws['C']
+def iterate_all_xlsx(inpath):
+    if os.path.isfile(inpath):
+        yield inpath
+    else:
+        for f in os.listdir(inpath):
+            if os.path.splitext(f)[1] == '.xlsx':
+                yield os.path.join(inpath, f)
+
+def retrieve_contents(inpath, outfile):
+    regex = re.compile('\r\n|\n')
     with open(outfile, 'w') as of:
-        regex = re.compile('\r\n|\n')
-        for cell in cells:
-            if cell.row < 1:
-                continue
-            of.write(regex.sub(' ', str(cell.value)) + '\n')
+        for infile in iterate_all_xlsx(inpath):
+            #print(infile)
+            wb = load_workbook(infile)
+            ws = wb.active
+            cells = ws['C']
+            #print(len(cells) - 1)
+            for cell in cells:
+                if cell.row < 2:
+                    continue
+                of.write(regex.sub(' ', str(cell.value)) + '\n')
 
--- tests/data_preparation/test_build_data.py
+++ tests/data_preparation/test_build_data.py
@@ -7,9 +7,8 @@ from nlpdemo.data_preparation.build_data import *
 class TestStringMethods(unittest.TestCase):
 
     def test_retrieve_contents(self):
-        retrieve_contents('data/yahoo_comment_xlsx/T_SNS_YAHOO_0914-1020_STOCK-1408_sjis.xlsx', \
-                    'result/yahoo_comment_xlsx/T_SNS_YAHOO_0914-1020_STOCK-1408_sjis.txt')
-        #self.assertEqual('', '')
+        retrieve_contents('data/yahoo_comment_xlsx/', \
+                    'result/yahoo_comment_xlsx/all_contents.txt')
 
 if __name__ == '__main__':
    unittest.main()
