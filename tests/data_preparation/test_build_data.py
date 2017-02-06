6139772af45077a5f9744c062f97a5ab202cf1c9
JuneQ Cheung <a1624jz@aiit.ac.jp>
2017-02-06 17:52:01 +0900

    Add machine filter

--- tests/data_preparation/test_build_data.py
+++ tests/data_preparation/test_build_data.py
@@ -6,9 +6,17 @@ from nlpdemo.data_preparation.build_data import *
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_retrieve_contents(self):
+    def test_build_data_01(self):
+        print('test retrieve_contents start...')
         retrieve_contents('data/yahoo_comment_xlsx/', \
-                    'result/yahoo_comment_xlsx/all_contents.txt')
+                    'work/yahoo_comment_xlsx/all_contents.raw')
+        print('test retrieve_contents end...')
+
+        print('test machine_filter start...')
+        machine_filter('work/yahoo_comment_xlsx/all_contents.raw', \
+                    'work/yahoo_comment_xlsx/all_contents.cls', \
+                    'data/yahoo_comment.kws')
+        print('test machine_filter end...')
 
 if __name__ == '__main__':
    unittest.main()
